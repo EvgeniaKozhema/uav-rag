885eeb1bbaf5d934c04bd093b9e676f5b77144a3
Evgenia Felagereva <efelagereva@gmail.com>
2025-07-08 19:59:55 +0300

    Get embeddings

--- scripts/split_and_embed.py
+++ scripts/split_and_embed.py
@@ -1,2 +1,64 @@
 import os
 import json
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import Chroma
+
+INPUT_DIR = "data/processed"
+CHROMA_DIR = "embeddings/chroma_db"
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=200,
+    separators=["\n\n", "\n", ".", " ", ""]
+)
+
+embedding_model = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2",
+    model_kwargs={"device": "cpu"},
+    encode_kwargs={"normalize_embeddings": True}
+)
+
+def load_documents():
+    documents = []
+
+    for filename in os.listdir(INPUT_DIR):
+        if not filename.endswith('.txt'):
+            continue
+
+        filepath = os.path.join(INPUT_DIR, filename)
+
+        with open(filepath, 'r', encoding='utf-8') as f:
+            text = f.read()
+
+        chunks = text_splitter.split_text(text)
+
+        for idx, chunk in enumerate(chunks):
+            documents.append({
+                'text':chunk,
+                'metadata':{'sourse':filename, 'chunk_id':idx}
+            })
+    return documents
+
+def build_vectorstore(documents):
+    texts = [doc['text'] for doc in documents]
+    metadatas = [doc['metadata'] for doc in documents]
+
+    vectorstore = Chroma.from_texts(
+        texts=texts,
+        embedding=embedding_model,
+        metadatas=metadatas,
+        persist_directory=CHROMA_DIR
+    )
+    vectorstore.persist()
+
+def main():
+    documents = load_documents()
+    build_vectorstore(documents)
+
+if __name__=='__main__':
+    main()
+
+
+

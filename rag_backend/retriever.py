8006e4aaee6ce505be179b78a907d25f1e0a5f69
Evgenia Felagereva <efelagereva@gmail.com>
2025-08-14 11:54:54 +0300

    Update retriever.py

--- rag_backend/retriever.py
+++ rag_backend/retriever.py
@@ -1,6 +1,6 @@
 import os
-from langchain.vectorstores import Chroma
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
 
 CHROMA_DIR = "embeddings/chroma_db"
 
@@ -10,26 +10,9 @@ embedding_model = HuggingFaceEmbeddings(
     encode_kwargs={"normalize_embeddings": True}
 )
 
-vectorstore = Chroma(embedding_function=embedding_model, persist_directory=CHROMA_DIR)
-
-def get_relevant_chunk(query: str, k: int=5):
-    formatted_query = f"query: {query}"
-    docs = vectorstore.similarity_search(query, k=k)
-
-    results = []
-
-    for doc in docs:
-        results.append({
-            'text':doc.page_content,
-            'metadata':doc.metadata})
-
-    return results
-
-if __name__=='__main__':
-    query = input('Введите ваш вопрос:')
-    chunks = get_relevant_chunk(query)
-
-    for i, chunk in enumerate(chunks, 1):
-        print(f"\n--- Чанк {i} ---")
-        print(f"Источник: {chunk['metadata'].get('source')} | ID: {chunk['metadata'].get('chunk_id')}")
-        print(chunk["text"])
+def get_vectorstore():
+    vectorstore = Chroma(
+        embedding_function=embedding_model,
+        persist_directory=CHROMA_DIR
+    )
+    return vectorstore
